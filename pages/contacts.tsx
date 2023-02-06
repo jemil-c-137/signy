c81568c3adea40a38d273bd8832da4da6f25143c
jemil-c-137 <jemil1931@gmail.com>
2023-02-06 22:58:51 +0100

    add contacts page

---
+++ pages/contacts.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/layout';
+
+export default function Contacts() {
+  return (
+    <Layout>
+      <main className='section pt-24 pb-24' ></main>
+    </Layout>
+  );
+}
