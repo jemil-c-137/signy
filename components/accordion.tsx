e094eee2209b5c6408cf44300ef1a34d7f5cd897
jemil-c-137 <jemil1931@gmail.com>
2023-02-12 21:05:25 +0100

    refactor

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -1,5 +1,5 @@
-import React, { JSXElementConstructor, MouseEventHandler, ReactNode, useState } from 'react';
-
+import React, { useState } from 'react';
+import Plus from '../assets/logos/plus.svg';
 interface IAccordionProps {
   list: {
     title: string;
@@ -13,6 +13,10 @@ const Accordion: React.FC<IAccordionProps> = ({ list }) => {
   const handleClick = (event: React.MouseEvent<HTMLDivElement>, idx: number) => {
     event.stopPropagation();
 
+    const element = event.target as HTMLElement;
+
+    console.log(element, 'item');
+
     if (idx === active) {
       setActive(-1);
     } else {
@@ -24,13 +28,15 @@ const Accordion: React.FC<IAccordionProps> = ({ list }) => {
     <div className="flex flex-col items-start justify-center gap-y-8">
       {list.map((item, index) => (
         <div key={item.title} onClick={(e) => handleClick(e, index)}>
-          <div className="flex gap-x-4 cursor-pointer">
-            <div>{active === index ? '-' : '+'}</div>
-            <div>{item.title}</div>
+          <div className="flex gap-x-4 cursor-pointer items-center">
+            <div>
+              <Plus width={16} height={16} className={`text-success plus ${active === index ? 'minus' : ''}`} />
+            </div>
+            <div className={`${active === index ? 'font-bold' : ''} text-xl transition-all`}>{item.title}</div>
           </div>
-          {index === active && (
-            <div className="accordion-content" dangerouslySetInnerHTML={{ __html: item.content }}></div>
-          )}
+          <div className={`accordion-content ml-8 ${active === index ? 'mt-4' : ''}`} aria-expanded={active !== index}>
+            <div dangerouslySetInnerHTML={{ __html: item.content }}></div>
+          </div>
         </div>
       ))}
     </div>
